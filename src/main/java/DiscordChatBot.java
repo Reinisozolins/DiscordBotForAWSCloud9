64d5e8790060af15e22ed5579fefd5d7ce0b8549
ReinisOzolins <reinisozolins@mail.com>
2020-07-16 11:57:41 +0300

    Test on AWS Cloud9

--- src/main/java/DiscordChatBot.java
+++ src/main/java/DiscordChatBot.java
@@ -1,11 +1,11 @@
-import events.FirstEvent;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class DiscordChatBot {
     public static void main(String[] args) throws Exception{
 //testbot
-        JDA jda = new JDABuilder("token").build();
+        JDA jda = new JDABuilder("NzMyNzIxMjM4NTUxNjkxNDE1.Xw4t6g.CL_OpOrT3hXjeV_L5LpB-WwxGnQ").build();
 
         jda.addEventListener(new FirstEvent());
     }
